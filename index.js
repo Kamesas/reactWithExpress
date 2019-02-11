ae74c260cd6a587ef8162bdfe4d39db12d93d8df
Kamesas <semakaleksandr2014@gmail.com>
2019-02-11 13:13:08 +0200

    CRUD functions ready

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const mongoos = require("mongoose");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
-mongoos.connect("mongodb://localhost/muggers-db"); //приконектили,создали и назвали БД
+mongoose.connect("mongodb://localhost/muggers-db", { useNewUrlParser: true }); //приконектили,создали и назвали БД
 
+app.use(bodyParser.json());
 app.use("/api", require("./api")); //импортировали API с api.js
 
 app.listen(4000, () => {
