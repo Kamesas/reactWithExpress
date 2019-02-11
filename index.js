cf8a6202c37b6b094c4f3fa230c58daf723bc1f6
Kamesas <semakaleksandr2014@gmail.com>
2019-02-11 11:43:47 +0200

    connected with mongo DB

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const mongoos = require("mongoose");
 
 const app = express();
+mongoos.connect("mongodb://localhost/muggers-db"); //приконектили,создали и назвали БД
 
-app.use("/api", require("./api"));
+app.use("/api", require("./api")); //импортировали API с api.js
 
 app.listen(4000, () => {
   console.log("server Express is listening !");
